770e386113767e8034849864ac496ae69b12a452
Yanique Brooks <yaniquebro@gmail.com>
2024-07-23 19:17:18 -0400

    index variables

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const express = require('express');
 const inquirer = require('inquirer');
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
